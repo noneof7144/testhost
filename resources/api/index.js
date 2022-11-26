650a7a69496c8951ba01d9777cf6c64db2b76ebc
noneof7144 <44343203+noneof7144@users.noreply.github.com>
2022-11-26 18:58:57 +0900

    t

--- resources/api/index.js
+++ resources/api/index.js
@@ -1,6 +1,6 @@
 const jsonData=require('./users.json');
 exports.handler = async (event) => {
-
+  console.log(JSON.stringify(event));
   return {
     statusCode: 200,
     headers:{ 'Content-Type': 'text/plain;charset=utf-8'},
