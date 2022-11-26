93cf3e87960c5596f0ee1a4bc0dd2a73958ced35
noneof7144 <44343203+noneof7144@users.noreply.github.com>
2022-11-26 18:55:46 +0900

    tes

--- resources/api/index.js
+++ resources/api/index.js
@@ -1,7 +1,10 @@
-exports.handler = async () => {
+const jsonData=require('./users.json');
+exports.handler = async (event) => {
+
   return {
     statusCode: 200,
-    body: 'Hello World',
+    headers:{ 'Content-Type': 'text/plain;charset=utf-8'},
+    body: 'Hello World'+JSON.stringify(jsonData)+JSON.stringify(event.body),
   };
 };
 
