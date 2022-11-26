8a7749174d0b82b1e7111dcea5fadbf3c09fd5bd
noneof7144 <44343203+noneof7144@users.noreply.github.com>
2022-11-26 19:06:57 +0900

    singn

---
+++ resources/api/signup.js
@@ -0,0 +1,15 @@
+const jsonData=require('./users.json');
+exports.handler = async (event) => {
+  console.log(JSON.stringify(event));
+  const userid=event.body.userid;
+  const passw=event.body.password;
+  return {
+    statusCode: 200,
+
+    headers:{ 'Content-Type': 'text/plain;charset=utf-8'},
+    body: JSON.stringify(
+	    { user_id : userid, password: passw}
+    ),
+  };
+};
+
